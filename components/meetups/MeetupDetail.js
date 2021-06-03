ebc910f9b9713e6a0002908ef743438eb2c20de9
felipemuner <felipe.muner@gmail.com>
2021-06-03 11:21:17 +0700

    mouseenter mouseleave - effect to change style

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,14 +1,21 @@
-import classes from './MeetupDetail.module.css'
+import classes from "./MeetupDetail.module.css";
+import { useState } from "react";
 
 function MeetupDetails(props) {
-  console.log(props)
+  const [style, setStyle] = useState({ color: "black" });
+  
   return (
-    <section className={classes.detail}>
-      <img
-        src={props.image}
-        alt=""
-      />
-      <h1>{props.title}</h1>
+    <section
+      className={classes.detail}
+      onMouseEnter={(e) => {
+        setStyle({ color: "blue" });
+      }}
+      onMouseLeave={(e) => {
+        setStyle({ color: "black" });
+      }}
+    >
+      <img src={props.image} alt="" />
+      <h1 style={style}>{props.title}</h1>
       <address>{props.address}</address>
     </section>
   );
