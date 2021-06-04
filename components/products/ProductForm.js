2ee1db39d5c41f5872b88052cc591c858ab8ca75
felipemuner <felipe.muner@gmail.com>
2021-06-04 09:17:51 +0700

    setting form with function onupdate and usestate

--- components/products/ProductForm.js
+++ components/products/ProductForm.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
@@ -5,41 +6,75 @@ import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
 
 function ProductForm(props) {
-  console.log(props);
+  const [query, setQuery] = useState({
+    name: "",
+    price: 0,
+    category: "",
+  });
+
+  // Update inputs value
+  const handleParam = () => (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setQuery((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
   async function handleSubmit(event) {
     event.preventDefault();
-    const res = await fetch("/api/products", {
-      body: JSON.stringify({
-        name: event.target.name.value,
-        price: event.target.price.value,
-        category: event.target.category.value,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-    });
+    try {
+      return await fetch("/api/products", {
+        body: JSON.stringify(query),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      }).then(async (res) => {
+        console.log(await res.json())
+        setQuery({ name: "", price: "", category: "" })
+      });
+    } catch (error) {
+      console.log(error)
+    }    
 
-    console.log(await res.json());
+    // console.log(await res.json());
   }
 
   return (
     <Box>
       <form onSubmit={handleSubmit}>
         <Box>
-          <TextField id="name" label="Name" fullWidth autoComplete="none" />
-        </Box>
-        <Box>
-          <TextField id="price" label="Price" fullWidth autoComplete="none" />
+          <TextField
+            name="name"
+            label="Name"
+            fullWidth
+            autoComplete="none"
+            value={query.name}
+            onChange={handleParam()}
+          />
         </Box>
         <Box>
           <TextField
-            id="category"
+            name="price"
+            label="Price"
+            fullWidth
+            autoComplete="none"
+            value={query.price}
+            onChange={handleParam()}
+          />
+        </Box>
+        <Box>
+          <TextField
+            name="category"
             label="Category"
             fullWidth
             autoComplete="none"
+            value={query.category}
+            onChange={handleParam()}
           />
         </Box>
+        {JSON.stringify(query)}
         <Box mt={2}>
           <Button variant="contained" color="primary" type="submit">
             Submit
