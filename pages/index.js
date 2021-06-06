431a4deec329b8adcfa49f2317a5d3c6a151da14
felipemuner <felipe.muner@gmail.com>
2021-06-06 18:01:35 +0700

    group by function object of object

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,18 @@
 import Head from "next/head";
-import { MongoClient } from "mongodb"; // only used on server side
-import MeetupList from "../components/meetups/MeetupList";
-import { STRING_CON } from "../constants";
 import { Fragment } from "react";
 // import useSwr from 'swr'
 // const fetcher = (url) => fetch(url).then((res) => res.json())
 
-function HomePage({ meetups }) {
+function HomePage(props) {
+  console.log(props)
   return (
     <Fragment>
-      <Head>
-        <title>HomePage - Meetups</title>
+      <Head>        
+        <title>Felipe Muner - Portifolio</title>
         <meta name="description" content="Meetups around the world." />
       </Head>
-      <MeetupList meetups={meetups} />;
+      {JSON.stringify(props)}
+
     </Fragment>
   );
 }
@@ -27,33 +26,32 @@ function HomePage({ meetups }) {
 //     props: {
 //       meetups: DUMMY_MEETUPS,
 //     }
-//   }
+//   }  
 // }
 
-export async function getStaticProps() {
-  //PRE GENERATED - NOT ALL INCOMING REQUEST, REFRESH 10SECOND
-  const client = await MongoClient.connect(STRING_CON, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  const db = client.db();
-  const meetupsCollection = db.collection("meetups");
+// export async function getStaticProps() {
+  // const client = await MongoClient.connect(STRING_CON, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // });
+  // const db = client.db();
+  // const meetupsCollection = db.collection("meetups");
 
-  const meetups = await meetupsCollection.find().toArray();
+  // const meetups = await meetupsCollection.find().toArray();
 
-  client.close();
+  // client.close();
 
-  return {
-    props: {
-      meetups: meetups.map((m) => ({
-        id: m._id.toString(),
-        title: m.title,
-        address: m.address,
-        image: m.image,
-      })),
-    },
-    revalidate: 10,
-  };
-}
+  // return {
+  //   props: {
+  //     meetups: meetups.map((m) => ({
+  //       id: m._id.toString(),
+  //       title: m.title,
+  //       address: m.address,
+  //       image: m.image,
+  //     })),
+  //   },
+  //   revalidate: 10,
+  // };
+// }
 
 export default HomePage;
--- pages/sellers.js
+++ pages/sellers.js
@@ -1,5 +1,52 @@
+const findByKey = (array, cur, key) => array.find((a) => a[key] === cur[key]);
+
+function getPageData(dayTrade, pageSize, pageNumber) {
+  const firstItem = pageSize * pageNumber - pageSize;
+  const lastItem = pageSize * pageNumber - 1;
+  // const uniquePeople = [...new Set(dayTrade.map(item => item.user))];
+  // console.log(dayTrade);
+  const uniquePeople = dayTrade.reduce((acc, cur) => {
+    const found = findByKey(acc, cur, "user");
+    found
+      ? (found.countOfStocks = found.countOfStocks + cur.countOfStocks)
+      : acc.push(cur);
+    return acc;
+  }, []);
+
+  const groupByUser = dayTrade.reduce(function (r, a) {
+    r[a.user] = r[a.user] || [];
+    r[a.user].push(a);
+    return r;
+  }, {});
+
+
+  const paginated = uniquePeople.filter(
+    (up, i) => i >= firstItem && i <= lastItem
+  );
+
+  const sorted = paginated.sort((a, b) => b.countOfStocks - a.countOfStocks);
+  return sorted;
+}
+
+var dayTrade = [
+  { id: 10, user: "Bill", company: "Goldman", countOfStocks: 512 },
+  { id: 1, user: "Rob", company: "Google", countOfStocks: 1 },
+  { id: 2, user: "Bill", company: "Goldman", countOfStocks: 2 },
+  { id: 3, user: "John", company: "JPMorgan", countOfStocks: 4 },
+  { id: 4, user: "Ruy", company: "Google", countOfStocks: 8 },
+  { id: 5, user: "Dina", company: "Goldman", countOfStocks: 16 },
+  { id: 7, user: "Marilin", company: "Google", countOfStocks: 64 },
+  { id: 8, user: "Bill", company: "Goldman", countOfStocks: 128 },
+  { id: 6, user: "Felipe", company: "JPMorgan", countOfStocks: 32 },
+  { id: 9, user: "Rob", company: "JPMorgan", countOfStocks: 256 },
+  { id: 11, user: "Rob", company: "JPMorgan", countOfStocks: 1024 },
+];
+
+
 
 function Sellers() {
+  const felipe = getPageData(dayTrade, 3, 2);
+  console.log(felipe)
   return <div>i'm Sellers</div>
 }
 
