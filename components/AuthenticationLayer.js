d18400150d661e9cfb2f808e3b82cbd7ffd932fc
felipemuner <felipe.muner@gmail.com>
2021-06-08 18:37:17 +0700

    set up login page

--- components/AuthenticationLayer.js
+++ components/AuthenticationLayer.js
@@ -1,37 +1,11 @@
-import { usersStore, usersDispatch } from "../store/users";
-import Button from "@material-ui/core/Button";
+import { usersStore } from "../store/users";
 import Login from "./Login";
 
 export default function AuthenticationLayer({ children }) {
   const users = usersStore();
-  const dispatchUser = usersDispatch();
-  console.log(users);
-  
-  const login = () => {
-    dispatchUser({
-      type: "LOGIN",
-    });
-  };
-
-  const logout = () => {
-    dispatchUser({
-      type: "LOGOUT",
-    });
-  };
 
   return (
     <div>
-      <h1> Auth layer</h1>
-      <Button onClick={() => login()} color="primary">
-        Log in
-      </Button>
-      <Button onClick={() => logout()} color="primary">
-        Log out
-      </Button>
-
-      {"felipe" + " - " + users.loggedIn}
-      <br />
-      <hr />
       {users.loggedIn ? children : <Login />}
     </div>
   );
