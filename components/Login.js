2eb8e6db656eb7b87c6c35267395aee111d8c23a
felipemuner <felipe.muner@gmail.com>
2021-06-08 19:30:30 +0700

    request by fetch put and delete

--- components/Login.js
+++ components/Login.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import {useEffect} from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+import Api from "../constants/api";
 
 import { usersStore, usersDispatch } from "../store/users";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -31,16 +33,16 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -52,13 +54,33 @@ export default function SignIn() {
   const users = usersStore();
   const dispatchUser = usersDispatch();
 
-
+  useEffect(() => {
+    async function fetchPutApi() {
+      const res = await Api.put({
+        url: "/api/products",
+        data: { felipe: 987654 },
+      });
+  
+      const products = await res.json();
+    }
+    fetchPutApi();
+  
+    async function fetchDeleteApi() {
+      const res = await Api.delete({
+        url: "/api/products",
+        data: { felipe: 987654 },
+      });
+  
+      const products = await res.json();
+    }
+    fetchDeleteApi();  
+  }, []);  
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatchUser({
       type: "LOGIN",
     });
-  }
+  };
   const classes = useStyles();
 
   return (
--- pages/api/products.js
+++ pages/api/products.js
@@ -20,6 +20,15 @@ async function handler(req, res) {
     res.status(201).json({ message: "Product inserted!" });    
   } else if (req.method === "GET") {
     res.status(200).json(products);
+  } else if (req.method === "PUT") {
+    console.log('putinz')
+    console.log(req.body)
+    res.status(200).json(products);
+
+  } else if (req.method === "DELETE") {
+    console.log('DELETE')
+    console.log(req.query)
+    res.status(200).json(products);
   }
 }
 
