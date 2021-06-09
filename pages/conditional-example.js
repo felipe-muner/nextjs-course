7681a490cfa109b638346d29eb173efa3134e079
felipemuner <felipe.muner@gmail.com>
2021-06-09 16:42:05 +0700

    map and return jsx

--- pages/conditional-example.js
+++ pages/conditional-example.js
@@ -18,7 +18,8 @@ const IfElseComp = (props) => {
 export default function Conditional() {  
   const [isLogged, setIsLogged] = useState(false);
   // const count = 0
-const count = 1
+  const count = 0
+  const felipeArray  = [1,2,3,4,5,6,7,8,9].map(el => <li>{el * 4}</li>)
   const toggleLogin = () => {
     setIsLogged(prev => !prev)
     console.log("this is:", isLogged);
@@ -30,6 +31,7 @@ const count = 1
       <button onClick={() => toggleLogin()}>toggle activity</button>
       <div>
       { count && <h1>Messages: {count}</h1>}
+      <ul>{felipeArray}</ul>
     </div>
     </>
   );
