f4c5da3d519b5286f78da2d64aa455e972821b89
felipemuner <felipe.muner@gmail.com>
2021-06-09 13:07:54 +0700

    Class Component - set function to update count on click

--- pages/turn-class.js
+++ pages/turn-class.js
@@ -1,9 +1,19 @@
 import { Component } from "react";
+import Button from "@material-ui/core/Button";
+class Felipe extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // console.log(this.props, 'felipe');
+    return <div>{JSON.stringify(this.props)}</div>;
+  }
+}
 
 class Clock extends Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = { date: new Date(), counter: 0 };
   }
 
   componentDidMount() {
@@ -16,7 +26,16 @@ class Clock extends Component {
 
   tick() {
     this.setState({
-      date: new Date()
+      date: new Date(),
+    });
+  }
+
+  felipe() {
+    this.setState((state) => {
+      console.log(state)
+      return {
+        counter: state.counter + 1,
+      };
     });
   }
 
@@ -26,6 +45,9 @@ class Clock extends Component {
         <h1>Hello, world!</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
         <h3>{this.state.timerID}</h3>
+        <h3>{JSON.stringify(this.state)}</h3>
+        <Felipe name={"felip2e"} />
+        <button onClick={() => this.felipe()}>change counter</button>
       </div>
     );
   }
